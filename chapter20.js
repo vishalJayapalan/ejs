83a9bf15de7db8820d251d433ff8b058e797f585
vishalJayapalan <vishal935@gmail.com>
2020-01-10 08:52:27 +0530

    after 20

---
+++ chapter20.js
@@ -0,0 +1,36 @@
+const { statSync, readdirSync, readFileSync } = require('fs')
+
+let searchTerm = new RegExp(process.argv[2])
+
+for (let arg of process.argv.slice(3)) {
+  search(arg)
+}
+
+function search (file) {
+  let stats = statSync(file)
+  if (stats.isDirectory()) {
+    for (let f of readdirSync(file)) {
+      search(file + '/' + f)
+    }
+  } else if (searchTerm.test(readFileSync(file, 'utf8'))) {
+    console.log(file)
+  }
+}
+
+// second exercise
+
+const { mkdir } = require('fs').promises
+
+methods.MKCOL = async function (request) {
+  let path = urlPath(request.url)
+  let stats
+  try {
+    stats = await stat(path)
+  } catch (error) {
+    if (error.code != 'ENOENT') throw error
+    await mkdir(path)
+    return { status: 204 }
+  }
+  if (stats.isDirectory()) return { status: 204 }
+  else return { status: 400, body: 'Not a directory' }
+}
