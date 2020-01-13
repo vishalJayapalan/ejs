7ec4726f6cbcd533bb31468586a56eb62977616f
vishalJayapalan <vishal935@gmail.com>
2020-01-13 21:09:11 +0530

    after 21

--- chapter20.js
+++ chapter20.js
@@ -19,18 +19,18 @@ function search (file) {
 
 // second exercise
 
-const { mkdir } = require('fs').promises
+// const { mkdir } = require('fs').promises
 
-methods.MKCOL = async function (request) {
-  let path = urlPath(request.url)
-  let stats
-  try {
-    stats = await stat(path)
-  } catch (error) {
-    if (error.code != 'ENOENT') throw error
-    await mkdir(path)
-    return { status: 204 }
-  }
-  if (stats.isDirectory()) return { status: 204 }
-  else return { status: 400, body: 'Not a directory' }
-}
+// methods.MKCOL = async function (request) {
+//   let path = urlPath(request.url)
+//   let stats
+//   try {
+//     stats = await stat(path)
+//   } catch (error) {
+//     if (error.code != 'ENOENT') throw error
+//     await mkdir(path)
+//     return { status: 204 }
+//   }
+//   if (stats.isDirectory()) return { status: 204 }
+//   else return { status: 400, body: 'Not a directory' }
+// }
